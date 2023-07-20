1c9c79b4b717d4e20c27d1512bff3e5e583a4341
Iwamgad <dagmawiharry2@gmail.com>
2023-07-20 10:50:01 +0300

    Adds the files

--- Spring-data-jpa-project/src/main/java/com/portfolio/Spring/data/jpa/project/entity/Student.java
+++ Spring-data-jpa-project/src/main/java/com/portfolio/Spring/data/jpa/project/entity/Student.java
@@ -1,2 +1,29 @@
-package com.portfolio.Spring.data.jpa.project.entity;public class Student {
+package com.portfolio.Spring.data.jpa.project.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "tbl_student")
+public class Student {
+    @Id
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+
+    @Column
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
 }
